2cdc6fceec197ed6434571e4f532dbf57e2aeb71
rykinajane-alt <rykinajane@gmail.com>
2026-03-20 20:39:09 +0300

    i'm creating a folder for the second task

---
+++ part1_2/EmployeeFixed.java
@@ -0,0 +1,54 @@
+package part1_2;
+
+public class EmployeeFixed {
+    private String name;
+    private int age;
+    private double salary;
+    private String password;
+
+    public EmployeeFixed(String name, int age, double salary, String password) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+        this.password = password;
+    }
+
+    // Геттеры для полей (без password)
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getRole() {
+        return "Employee";
+    }
+
+    // Публичный метод для повышения зарплаты
+    public void promote(double raise) {
+        if (raise > 0) {
+            this.salary += raise;
+        }
+    }
+
+    // Публичный метод для вывода информации
+    public void printSummary() {
+        System.out.println(name + ", " + age + " лет, " + salary + " руб.");
+    }
+
+    // Приватный метод проверки пароля
+    private boolean validatePassword(String input) {
+        return password.equals(input);
+    }
+
+    // Публичный метод аутентификации
+    public boolean authenticate(String input) {
+        return validatePassword(input);
+    }
+}
