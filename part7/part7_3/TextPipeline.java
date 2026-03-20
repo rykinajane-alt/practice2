57c3e60892707e6a520b4e038d37bc97f3676d5b
rykinajane-alt <rykinajane@gmail.com>
2026-03-20 21:35:45 +0300

    i did part7_3

--- part7/part7_3/TextPipeline.java
+++ part7/part7_3/TextPipeline.java
@@ -29,16 +29,25 @@ public class TextPipeline {
         //   Function<String, String> removeExtraSpaces = s -> s.replaceAll("\\s+", " ");
         //   Function<String, String> capitalize = s -> s.isEmpty() ? s : Character.toUpperCase(s.charAt(0)) + s.substring(1);
 
+        Function<String, String> trim = String::trim;
+        Function<String, String> lower = String::toLowerCase;
+        Function<String, String> removeExtraSpaces = s -> s.replaceAll("\\s+", " ");
+        Function<String, String> capitalize = s -> s.isEmpty() ? s : Character.toUpperCase(s.charAt(0)) + s.substring(1);
+
         // TODO: скомпонуйте в одну функцию:
         //   var normalize = trim.andThen(lower).andThen(removeExtraSpaces).andThen(capitalize);
 
+        var normalize = trim.andThen(lower).andThen(removeExtraSpaces).andThen(capitalize);
+
         // TODO: примените к нескольким строкам:
         String[] testStrings = {
-            "  пРИВЕТ    МИР  ",
-            "   jAVA   пРОГРАММИРОВАНИЕ   ",
-            "ТЕСТ"
+                "  пРИВЕТ    МИР  ",
+                "   jAVA   пРОГРАММИРОВАНИЕ   ",
+                "ТЕСТ"
         };
-        // for (String s : testStrings) System.out.println("\"" + s + "\" → \"" + normalize.apply(s) + "\"");
+        for (String s : testStrings) {
+            System.out.println("\"" + s + "\" → \"" + normalize.apply(s) + "\"");
+        }
 
         // ▲ КОНЕЦ ВАШЕГО КОДА (Часть A) ▲
 
@@ -60,10 +69,36 @@ public class TextPipeline {
         //     }
         // }
 
+        class WordCounter {
+            private final String text;
+
+            WordCounter(String text) {
+                this.text = text;
+            }
+
+            Map<String, Integer> count() {
+                Map<String, Integer> map = new HashMap<>();
+                String[] words = text.split(" ");
+                for (String word : words) {
+                    map.merge(word, 1, Integer::sum);
+                }
+                return map;
+            }
+
+            String mostFrequent() {
+                return count().entrySet()
+                        .stream()
+                        .max(Map.Entry.comparingByValue())
+                        .map(Map.Entry::getKey)
+                        .orElse(null);
+            }
+        }
+
         // TODO: используйте WordCounter для анализа нормализованной строки:
-        // var wc = new WordCounter(normalize.apply("  java java PYTHON  java python  "));
-        // System.out.println("Частоты: " + wc.count());
-        // System.out.println("Самое частое: " + wc.mostFrequent());
+        var wc = new WordCounter(normalize.apply("  java java PYTHON  java python  "));
+        System.out.println("\nЧастоты: " + wc.count());
+        System.out.println("Самое частое: " + wc.mostFrequent());
+
         // ▲ КОНЕЦ ВАШЕГО КОДА (Часть B) ▲
     }
 }
